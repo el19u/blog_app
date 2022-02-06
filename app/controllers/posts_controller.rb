344eaa11ebcd984a6faff1430e39230f0dd5e363
elik <akainooo@gmail.com>
2022-02-06 13:54:26 +0800

    add posts new form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,49 @@
 class PostsController < ApplicationController
+  before_action :find_params, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:id])
+
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(posts_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy if @post
+    redirect_to root_path
   end
 
   private
   def posts_params
     params.require(:posts).permit(:title, :author, :content)
+  end
+
+  def find_params
+    @post = Post.find(params[:id])
   end
 end
